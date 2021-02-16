38b6a6d268299d97a137240f1232bb4d0bb880f4
Carlos Medina <carlosmbinf@gmail.com>
2021-02-16 15:23:36 -0600

    puerto 88

--- server/main.js
+++ server/main.js
@@ -313,7 +313,7 @@ var options = {
  ws: true,
  xfwd: true
 };
-var server = httpProxy.createProxyServer(options).listen(5000);
+var server = httpProxy.createProxyServer(options).listen(88);
 console.log('httpProxy running with target at ' + options.target);
 
 
