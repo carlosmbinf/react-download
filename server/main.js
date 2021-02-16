f47929d3c28380e099e25f5f705cc53c97fac189
Carlos Medina <carlosmbinf@gmail.com>
2021-02-16 12:55:10 -0600

    change port 3002

--- server/main.js
+++ server/main.js
@@ -282,7 +282,7 @@ if (Meteor.isServer) {
   });
   var appRoot = require("app-root-path");
   SSLProxy({
-    port: 6000, //or 443 (normal port/requires sudo)
+    port: 3002, //or 443 (normal port/requires sudo)
     ssl : {
       key: fs.readFileSync(appRoot.path + '/server/conf/key.pem'),
       cert: fs.readFileSync(appRoot.path + '/server/conf/cert.pem')
