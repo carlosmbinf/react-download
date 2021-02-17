4c7076b2100a1fb3020ee95fda887b349030fd8b
Carlos Medina <carlosmbinf@gmail.com>
2021-02-17 09:13:29 -0600

    add ROOT_URL in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
     script: "meteor run",
     env: {
       NODE_ENV: "development",
+      "ROOT_URL": "https://srv5119-206152.vps.etecsa.cu:5000/",
+      "PORT": 3000,
     },
     env_production: {
       NODE_ENV: "production",
