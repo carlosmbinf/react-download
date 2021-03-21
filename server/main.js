8a0a46416c28575fe0cd645c452e3df64ac90ce0
Carlos Medina <carlosmbinf@gmail.com>
2021-03-21 18:08:55 -0400

    pro with users and password bob 123

--- server/main.js
+++ server/main.js
@@ -383,16 +383,94 @@ var options = {
 var server = httpProxy.createProxyServer(options).listen(5000);
 console.log('httpProxy running with target at ' + options.target);
 
-const proxy = require('@ucipass/proxy')
-const proxyPort = 3002
-proxy(proxyPort)
-  .then(() => {
-    // Use it for a while....
-  })
-  .then((server) => {
-    // console.log(server);
-    // server.stop() 
-  })
+// -------------------Este Proxy Funciona al FULLLLLLLLL-----------
+// const proxy = require('@ucipass/proxy')
+// const proxyPort = 3002
+// proxy(proxyPort)
+//   .then(() => {
+//     // Use it for a while....
+//   })
+//   .then((server) => {
+//     // console.log(server);
+//     // server.stop() 
+//   })
+
+// -------------------Este Proxy Funciona al FULLLLLLLLL-----------
+
+
+
+
+
+
+const ProxyChain = require('proxy-chain');
+
+const server2 = new ProxyChain.Server({
+    // Port where the server will listen. By default 8000.
+    port: 3002,
+
+    // Enables verbose logging
+    // verbose: true,
+
+    // Custom user-defined function to authenticate incoming proxy requests,
+    // and optionally provide the URL to chained upstream proxy.
+    // The function must return an object (or promise resolving to the object) with the following signature:
+    // { requestAuthentication: Boolean, upstreamProxyUrl: String }
+    // If the function is not defined or is null, the server runs in simple mode.
+    // Note that the function takes a single argument with the following properties:
+    // * request      - An instance of http.IncomingMessage class with information about the client request
+    //                  (which is either HTTP CONNECT for SSL protocol, or other HTTP request)
+    // * username     - Username parsed from the Proxy-Authorization header. Might be empty string.
+    // * password     - Password parsed from the Proxy-Authorization header. Might be empty string.
+    // * hostname     - Hostname of the target server
+    // * port         - Port of the target server
+    // * isHttp       - If true, this is a HTTP request, otherwise it's a HTTP CONNECT tunnel for SSL
+    //                  or other protocols
+    // * connectionId - Unique ID of the HTTP connection. It can be used to obtain traffic statistics.
+    prepareRequestFunction: ({ request, username, password, hostname, port, isHttp, connectionId }) => {
+      
+        return {
+            // If set to true, the client is sent HTTP 407 resposne with the Proxy-Authenticate header set,
+            // requiring Basic authentication. Here you can verify user credentials.
+            requestAuthentication: username !== 'bob' || password !== '123',
+
+            // Sets up an upstream HTTP proxy to which all the requests are forwarded.
+            // If null, the proxy works in direct mode, i.e. the connection is forwarded directly
+            // to the target server. This field is ignored if "requestAuthentication" is true.
+            // The username and password should be URI-encoded, in case it contains some special characters.
+            // See `parseUrl()` function for details.
+            // upstreamProxyUrl: `http://username:password@proxy.example.com:3128`,
+
+            // If "requestAuthentication" is true, you can use the following property
+            // to define a custom error message to return to the client instead of the default "Proxy credentials required"
+            failMsg: 'Bad username or password, please try again.',
+        };
+    },
+});
+
+server2.listen(() => {
+  console.log(`Proxy server is listening on port ${server2.port}`);
+});
+
+
+// Emitted when HTTP connection is closed
+server2.on('connectionClosed', ({ connectionId, stats }) => {
+  // console.log(`Connection ${connectionId} closed`);
+  // console.dir(stats);
+});
+
+// Emitted when HTTP request fails
+server2.on('requestFailed', ({ request, error }) => {
+  console.log(`Request ${request.url} failed`);
+  // console.error(error);
+})
+
+
+
+
+
+
+
+
 
 
 
