1c1af8a3fa277849bca0e6b8669a84821570ffa4
Carlos Medina <carlosmbinf@gmail.com>
2023-08-06 17:59:42 -0300

    Cambiando a Back-End para separar infraestructura

--- client/main.jsx
+++ client/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import App from '/imports/layout/index';
-import LoginPage from '../imports/ui/pages/login/index';
-import { useTracker } from "meteor/react-meteor-data";
+// import App from '/imports/layout/index';
 
 
 
 Meteor.startup(() => {
   
-    render(<App />, document.getElementById('react-target'));
+    // render(<App />, document.getElementById('react-target'));
   
 });
