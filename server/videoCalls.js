5886cbba0e59d5b5b9d4006c606b0565a1b21824
Carlos Medina <carlosmbinf@gmail.com>
2025-02-15 17:04:20 -0300

    Actualizar origen CORS en el servidor de videollamadas para permitir solicitudes desde app.vidkar.com

--- server/videoCalls.js
+++ server/videoCalls.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",  // Permitir solicitudes del cliente Meteor (puerto 3000)
+    origin: "http://app.vidkar.com:3000",  // Permitir solicitudes del cliente Meteor (puerto 3000)
     methods: ["GET", "POST"],
   },
 });
