ec2e093455a448b359197a60afd7526876642a9b
Carlos Medina <carlosmbinf@gmail.com>
2025-02-21 00:59:11 -0300

    Actualizar observadores de usuarios VPN para utilizar 'vpnplusConnected' en lugar de 'username' y optimizar la consulta de campos.

--- server/observers.js
+++ server/observers.js
@@ -6,15 +6,14 @@ const Users = Meteor.users;
 
 Meteor.startup(() => {
     console.log("INICIANDO OBSERVERS");
-    Users.find({ vpn: true }).observeChanges({
+    Users.find({ vpn: true },{fields:{vpnplusConnected:1,_id:1}}).observeChanges({
         changed: async (id, fields) => {
             if ('vpnplusConnected' in fields) { //vpnplusConnected
                 let usuario = Meteor.users.findOne(id, { fields: { _id: 1, username: 1, vpnplusConnected: 1, bloqueadoDesbloqueadoPor: 1 } });
                 NotificacionUsersConectadosVPNCollection.find({ userIdConnected: id }).forEach(notifica => {
                     let usuarioAdmin = usuario && Meteor.users.findOne(notifica.adminIdSolicitud, { fields: { _id: 1, username: 1, vpnplusConnected: 1, idtelegram: 1 } });
-                    console.log("usuarioAdmin", usuarioAdmin);
                     if (usuarioAdmin && Meteor.callAsync("estaRegistradoEnTelegram", usuarioAdmin._id)) {
-                        if (usuario.vpnplusConnected) {
+                        if (fields.vpnplusConnected) {
                             Meteor.callAsync("enviarMensajeDirecto", usuarioAdmin._id, notifica.mensajeaenviarConnected);
                         } else {
                             Meteor.callAsync("enviarMensajeDirecto", usuarioAdmin._id, notifica.mensajeaenviarDisconnected);
