887f0fd438ea32689debcc598372f86d27d8b963
Carlos Medina <carlosmbinf@gmail.com>
2025-02-21 00:46:49 -0300

    Eliminar consola de depuración en el observador de cambios de conexión VPN

--- server/observers.js
+++ server/observers.js
@@ -10,7 +10,6 @@ Meteor.startup(() => {
         changed: async (id, fields) => {
             if ('vpnplusConnected' in fields) { //vpnplusConnected
                 let usuario = Meteor.users.findOne(id, { fields: { _id: 1, username: 1, vpnplusConnected: 1, bloqueadoDesbloqueadoPor: 1 } });
-                console.log("usuario", usuario);
                 NotificacionUsersConectadosVPNCollection.find({ userIdConnected: id }).forEach(notifica => {
                     let usuarioAdmin = usuario && Meteor.users.findOne(notifica.adminIdSolicitud, { fields: { _id: 1, username: 1, vpnplusConnected: 1, idtelegram: 1 } });
                     console.log("usuarioAdmin", usuarioAdmin);
